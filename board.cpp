e0c382db3a36dd0e4a99a5e84ddeb164c26e87cd
Your Name <you@example.com>
2018-11-05 15:59:37 -0500

    added assignment operator and initiate board

--- board.cpp
+++ board.cpp
@@ -22,3 +22,42 @@ board::board(board &h)
 		my_board[i] = h.my_board[i];
 	}
 }
+
+//assignment operator
+board & board::operator = (const board &h)
+{
+	board_size = h.board_size;
+	my_board = new int*[board_size];
+	for(int i = 0; i < board_size; i++)
+	{
+		my_board[i] = h.my_board[i];
+	}
+	return *this;
+}
+
+//prompts the user for dimension of board
+void board::get_board_size()
+{
+	 cout << "\t\tWhat board size would you like?\n";
+	 cin >> board_size;
+
+	 //dynamic allocation
+	 my_board = new int* [board_size];
+	 for (int i = 0; i < board_size; ++i)
+	 {
+	 	my_board[i] = new int[board_size];
+	 }
+	 	
+	 //fill
+	 for (int row = 0; row < board_size; ++row)
+	 	for(int col = 0; col < board_size; ++col)
+	 		my_board[row][col] = 0;
+}
+
+//determines the position and value of two starting tiles
+void board::start_tiles()
+{
+	my_board[0][0] = 2;
+	my_board[1][1] = 2;
+	print_board(); 
+}
