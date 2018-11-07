c2ad6009aacc7d2568db06b1bc3d366d91204711
Your Name <you@example.com>
2018-11-06 20:36:08 -0500

    added print board and get move function

--- board.cpp
+++ board.cpp
@@ -61,3 +61,46 @@ void board::start_tiles()
 	my_board[1][1] = 2;
 	print_board(); 
 }
+
+//prompt the user for dimension of board
+//prints board to user
+void board::print_board()
+{
+		for (int col = 0; col < board_size; ++col)
+		{
+			for (int row = 0; row < board_size; ++row)
+			{
+				if (my_board[col][row] == 0)	
+					cout << "\tx\t";
+				else 
+					cout << "\t" << my_board[col][row] << "\t";
+				if ( row % board_size == board_size -1)
+					cout << "\n\n";
+			}
+		}
+		cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
+}
+
+//asks user to enter a move
+//returns the move (w, s, d, a, h, or q)
+//any other letter is an invalid move
+//the function calls itself if the letter is invalid
+char board::get_move()
+{	
+	bool invalid = false;
+		cout << "\t\tEnter your move:\n";
+		//allows user to play without pressing the return key
+		system("stty raw");
+		cin >> move;
+		cout << endl << endl;
+		if (move != 'w' && move != 's' && move != 'd' && move != 'a' && move != 'q' && move != 'h')
+		{
+			cout << "\t\tinvalid, move should be w, s, d, a, h, q, or x\n";
+			invalid = true;
+		}
+		system("stty cooked");
+		cout << endl;
+		if(invalid)
+			get_move();
+		return move;
+}			
